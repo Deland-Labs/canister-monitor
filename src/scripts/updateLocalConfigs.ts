e6437fe91b7b1aacadabe7c6b2138931a0a6f87a
vanilla <101264439+ne8u14@users.noreply.github.com>
2022-07-01 14:05:50 +0800

    update

--- src/scripts/updateLocalConfigs.ts
+++ src/scripts/updateLocalConfigs.ts
@@ -21,30 +21,14 @@ import {identity, dfxJson, canister} from '@deland-labs/ic-dev-kit';
     fs.writeFileSync(`${dir}/dev.canister_ids.env`, env_file_content);
 
     const admin = `COMMON_PRINCIPAL_NAME_ADMIN`;
-    const admin_v = identity.identityFactory.getPrincipal('icnaming_main')?.toText();
-    const exporter = `COMMON_PRINCIPAL_NAME_STATE_EXPORTER`;
-    const exporter_v = identity.identityFactory.getPrincipal('icnaming_state_exporter')?.toText();
-    const trigger = `COMMON_PRINCIPAL_NAME_TIMER_TRIGGER`;
-    const trigger_v = identity.identityFactory.getPrincipal('icnaming_timer_trigger')?.toText();
+    const admin_v = identity.identityFactory.getPrincipal('main')?.toText();
     const monitor = `COMMON_PRINCIPAL_NAME_MONITOR`;
-    const monitor_v = identity.identityFactory.getPrincipal('icnaming_monitor')?.toText();
+    const monitor_v = identity.identityFactory.getPrincipal('monitor')?.toText();
 
 
     const principalContent = `export ${admin}="
 # main node
 ${admin_v}
-"
-export ${exporter}="
-# main node
-${admin_v}
-# state exporter node
-${exporter_v}
-"
-export ${trigger}="
-# main node
-${admin_v}
-# timer_trigger node
-${trigger_v}
 "
 export ${monitor}="
 # main node
